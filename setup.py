0ae5fabb43e829b591c5d8cbf2363ebe1cc93c4e
Antonio Torres <antorres@redhat.com>
2022-01-12 11:19:19 +0100

    Become 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ipaperftest',
-    version='0.1',
+    version='0.2',
     namespace_packages=['ipaperftest', ],
     package_dir={'': 'src'},
     packages=[
