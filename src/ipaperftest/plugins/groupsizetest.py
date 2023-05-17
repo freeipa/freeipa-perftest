311d4d8313618bc8470c4bec2063454c68d8b774
Rob Crittenden <rcritten@redhat.com>
2023-05-17 12:54:46 -0400

    Allow creating one-level nested groups for the GroupSizeTest
    This will be used to determine the general overhead of using
    nested memberof.
    
    Also include a JSON result record.
    
    Signed-off-by: Rob Crittenden <rcritten@redhat.com>

--- src/ipaperftest/plugins/groupsizetest.py
+++ src/ipaperftest/plugins/groupsizetest.py
@@ -5,20 +5,26 @@
 import subprocess as sp
 from datetime import datetime
 
-from ipaperftest.core.constants import ANSIBLE_GROUPSIZETEST_SERVER_CONFIG_PLAYBOOK
-from ipaperftest.core.plugin import Plugin
+from ipaperftest.core.constants import (
+    SUCCESS,
+    ANSIBLE_GROUPSIZETEST_SERVER_CONFIG_PLAYBOOK
+)
+from ipaperftest.core.plugin import Plugin, Result
 from ipaperftest.plugins.registry import registry
 
 
 @registry
 class GroupSizeTest(Plugin):
+    """Create a lot of users, add them to a group, measure time to add one more.
+
+       :param: threads: overloaded option, but this # of members in the group
+       :param: sizelimit: I have no idea. This may be stripped. All previous tests
+                          used sizelimit=100, the default.
+    """
 
     def __init__(self, registry):
         super().__init__(registry)
         self.custom_logs = ["~/*group_add_member.log", ]
-
-    def generate_clients(self, ctx):
-        yield ()
 
     def run(self, ctx):
         # TODO: this should be moved to a resources folder
@@ -27,7 +33,8 @@ class GroupSizeTest(Plugin):
         # Configure server before execution
         args = {
             "threads": ctx.params["threads"],
-            "sizelimit": ctx.params["sizelimit"]
+            "sizelimit": ctx.params["sizelimit"],
+            "number_of_subgroups": ctx.params["number_of_subgroups"]
         }
         self.run_ansible_playbook_from_template(
             ANSIBLE_GROUPSIZETEST_SERVER_CONFIG_PLAYBOOK,
@@ -38,7 +45,10 @@ class GroupSizeTest(Plugin):
         self.run_ssh_command("ipa user-add --first tim --last user tuser1",
                              self.provider.hosts["server"], ctx)
 
-        command = "ipa group-add-member --users tuser1 allusers"
+        if ctx.params.get("number_of_subgroups", 0) > 0:
+            command = "ipa group-add-member --users tuser1 group0 --no-members"
+        else:
+            command = "ipa group-add-member --users tuser1 allusers --no-members"
         cmd = (
             r"echo {cmd} > ~/group_add_member.log;"
             r"/usr/bin/time -p {cmd} >> ~/group_add_member.log 2>&1;"
@@ -61,10 +71,24 @@ class GroupSizeTest(Plugin):
                 addtime = line.split()[1]
                 break
 
-        self.results_archive_name = "GroupTest-{}-{}-{}threads-{}sizelimit-{}real".format(
-            datetime.now().strftime("%FT%H%MZ"),
-            self.provider.server_image.replace("/", ""),
-            ctx.params["threads"],
-            ctx.params["sizelimit"],
-            addtime
-        )
+        if ctx.params["number_of_subgroups"]:
+            self.results_archive_name = "GroupSizeTest-" \
+                    "{}-{}-{}threads-{}sizelimit-{}subgroups-{}real".format
+            (
+                datetime.now().strftime("%FT%H%MZ"),
+                self.provider.server_image.replace("/", ""),
+                ctx.params["threads"],
+                ctx.params["sizelimit"],
+                ctx.params["number_of_subgroups"],
+                addtime
+            )
+        else:
+            self.results_archive_name = "GroupSizeTest-{}-{}-{}threads-{}sizelimit-{}real".format(
+                datetime.now().strftime("%FT%H%MZ"),
+                self.provider.server_image.replace("/", ""),
+                ctx.params["threads"],
+                ctx.params["sizelimit"],
+                addtime
+            )
+        yield Result(self, SUCCESS, msg="Total users %s, subgroups %s, time %s" %
+                     (ctx.params["threads"], ctx.params["number_of_subgroups"], addtime))
